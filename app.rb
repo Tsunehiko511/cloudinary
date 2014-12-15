7e37ef0f58b720940b8811c62da5ec33021c0c60
Tunehiko511 <simanezumi1989@gmail.com>
2014-12-16 00:29:55 +0900

    5

--- app.rb
+++ app.rb
@@ -1,21 +1,18 @@
-# Get the New Post form
-get "/new" do
- 
-  @title = "New Post"
-  @post = Post.new
-  erb :"new"
+require 'sinatra'
+
+get "/" do
+	@title = "New Post"
+	erb :index
 end
- 
+
 post "/posts" do
- 
- @filename = params[:uploaded_data][:filename]
- @type = params[:uploaded_data][:type]
- @name= params[:uploaded_data][:name]
- @tempfile = params[:uploaded_data][:tempfile]
- @head = params[:uploaded_data][:head]
- 
- uploads ={}
- uploads[:fish] = Cloudinary::Uploader.upload(@tempfile.path,:tags => "basic_sample")
- 
- @url = uploads[:fish]['url']
+	@filename = params[:uploaded_data][:filename]
+	@type = params[:uploaded_data][:type]
+	@name= params[:uploaded_data][:name]
+	@tempfile = params[:uploaded_data][:tempfile]
+	@head = params[:uploaded_data][:head]
+	uploads ={}
+	uploads[:fish] = Cloudinary::Uploader.upload(@tempfile.path,:tags => params[:uploaded_data][:filename])
+	@url = uploads[:fish]['url']
+	redirect '/'
 end
