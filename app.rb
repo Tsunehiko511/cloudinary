8239fad5b838bf19156ece3e610b078c2c14a5f6
Tunehiko511 <simanezumi1989@gmail.com>
2014-12-17 01:55:22 +0900

    v0.1

--- app.rb
+++ app.rb
@@ -7,29 +7,35 @@ require 'cloudinary/utils'
 require 'active_record'
 require 'sinatra/json'
 require './models/comment.rb'
+require './config.ru'
 require 'securerandom'
 
-if Cloudinary.config.api_key.blank?
-	require './config'
-end
 get "/" do
-	@title = "New Post"
-	@comments = Comment.all
-	erb :index
+	ActiveRecord::Base.connection_pool.with_connection do
+		begin
+			@title = "New Post"
+			@comments = Comment.all
+			erb :index
+		end
+	end
 end
 
 post "/posts" do
-	if params[:uploaded_data]
-		@filename = SecureRandom.uuid
-		Comment.create({:body => params[:body],:name => params[:username],:filename => @filename})
-		@tempfile = params[:uploaded_data][:tempfile]
-		uploads ={}
-		uploads[:fish] = Cloudinary::Uploader.upload(@tempfile.path, :public_id=> @filename, :format=>'jpg' ,:tags => "basic_sample")
-		@url = uploads[:fish]['url']
-	else
+	ActiveRecord::Base.connection_pool.with_connection do
+		begin
+			if params[:uploaded_data]
+				@filename = SecureRandom.uuid
+				Comment.create({:body => params[:body],:name => params[:username],:filename => @filename})
+				@tempfile = params[:uploaded_data][:tempfile]
+				uploads ={}
+				uploads[:fish] = Cloudinary::Uploader.upload(@tempfile.path, :public_id=> @filename, :format=>'jpg' ,:tags => "basic_sample")
+				@url = uploads[:fish]['url']
+			else
 				Comment.create({:body => params[:body],:name => params[:username]})
+			end
+			redirect '/'
+		end
 	end
-	redirect '/'
 end
 
 post "/delete" do
